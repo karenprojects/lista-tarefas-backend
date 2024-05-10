5713ee4561f993f18be0e84486ce210c417d9ad2
matheusp94 <matheus.p940@gmail.com>
2024-05-09 22:16:12 -0300

    corrigindo porta para 5000

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000; // Defina a porta do servidor
 
-// Rota para obter todas as tarefas
-app.get('/tarefas', (req, res) => {
-    // Lógica para obter e retornar todas as tarefas do banco de dados
-});
+app.use(express.json()); // Responsável por fazer o parse do corpo das requisições HTTP com formato JSON
+
+let tarefas = [];
 
 // Rota para adicionar uma nova tarefa
 app.post('/tarefas', (req, res) => {
     // Lógica para adicionar uma nova tarefa ao banco de dados
 });
 
+// Rota para obter uma tarefas específica
+app.get('/tarefas', (req, res) => {
+    // Lógica para obter e retornar todas as tarefas do banco de dados
+});
+
+// Rota para obter todas as tarefas
+app.get('/tarefas/:id', (req, res) => {
+    // Lógica para obter uma tarefas do banco de dados pelo seu ID
+});
 // Rota para editar uma tarefa existente
 app.put('/tarefas/:id', (req, res) => {
     // Lógica para editar uma tarefa no banco de dados com base no ID
@@ -22,6 +30,7 @@ app.delete('/tarefas/:id', (req, res) => {
     // Lógica para excluir uma tarefa do banco de dados com base no ID
 });
 
+// Inicie o servidor Express:
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta em http://localhost:${PORT}`);
 });
